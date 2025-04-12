c0bc6d5d02035f382d07715bd6e1c564779e2ef2
crassbrasil <147461200+crassbrasil@users.noreply.github.com>
2025-04-12 11:46:04 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+</script>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="images/favi.webp" type="image/png" />
+    <title>Ajude Ana Laura</title>
+    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="css/glide.core.min.css" />
+    <link rel="stylesheet" href="css/glide.theme.min.css" />
+    <link rel="stylesheet" href="css/modal.css" />
+    <script src="js/qrcode.min.js"></script>
+  </head>
+
+</script>
